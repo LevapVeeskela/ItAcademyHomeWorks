f492ddca33ad432e1cd7504affa7021f7084ffa4
veeskela\Admin <paulalexeev10@gmail.com>
2019-09-29 00:01:32 +0300

    +added HW_04 and finished task_1

---
+++ ItAcademyHomeWorks/Common/Constants/HomeWorkFour.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.Constants
+{
+    public static class HomeWorkFour
+    {
+        public const string TextEnglishAlphabetUnicodeInfo = "Английский алфавит из UnicodeTable";
+    }
+}
---
+++ ItAcademyHomeWorks/HW_04_Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using BusinessLogic.Services.HW_04;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hw = new FirstTaskService();
+            hw.ShowEnglishAlphabet();
+            hw.ShowEnglishAlphabetFromUnicodeTableDecNumber();
+            hw.ShowEnglishAlphabetUnicodeTableEscapeSequence();
+            hw.ShowEnglishAlphabetUnicodeTableLinq();
+            Console.ReadKey();
+        }
+    }
+}
