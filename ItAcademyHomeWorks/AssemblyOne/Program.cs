0f94f6f248d9986a6990b326c4c0edc7b81dc2e1
veeskela\Admin <paulalexeev10@gmail.com>
2019-10-13 22:43:20 +0300

    *added seven hw with example use access modifier for methods, properties, field and etc.

---
+++ ItAcademyHomeWorks/AssemblyOne/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AssemblyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mp = new MotorcyclePublic("123123", "123ssa333dsda", 300, 35, 20000);
+            var mi = new MotorcycleInternal("123123", "123ssa333dsda", 245, 33, 20000, 150, 80);
+            var mdi = new MotorcycleDefaultInternal("23123ss", "32111wqqwewqe", 200, 29);
+        }
+    }
+}
---
+++ ItAcademyHomeWorks/Infrastructure/Abstractions/HW_07/Interfaces/IMotorcycle.cs
@@ -0,0 +1,33 @@
+﻿namespace Infrastructure.Abstractions.HW_07.Interfaces
+{
+    public interface IMotorcycle
+    {
+        string RegistrationNumber { get; }
+
+        string VinNumber { get; }
+
+        ushort FuelQuantity { get; set; }
+
+        ushort MaxSpeed { get; }
+
+        ushort CurrentSpeed { get; set; }
+
+        bool IsStartEngine { get; set; }
+
+        ushort Acceleration { get; }
+
+        int ValueOdometer { get; set; }
+
+        bool StartUp();
+
+        void Run();
+
+        ushort GetCurrentSpeed();
+
+        bool Stop();
+
+        ushort GetFuelQuantity();
+
+        ushort ReplenishFuelQuantity(ushort fuelQuantity);
+    }
+}
